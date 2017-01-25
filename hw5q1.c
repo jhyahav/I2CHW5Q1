37f43023a99d24e97d86f22fb7c1f2f6a89a9705
jhyahav <jhyahav@campus.technion.ac.il>
2017-01-25 22:36:22 +0200

    Added documentation for most of the code
    Also changed some variable and function names and shortened the
    recursive function. However, the recursive function needs documentation
    and the auxiliary recursive function (currently called 'Func') needs a
    better name and documentation.

--- hw5q1.c
+++ hw5q1.c
@@ -11,57 +11,73 @@
 
 /* FUNCTIONS */
 char* SplitExpression(char* expression);
-void StringCopy(char *source, char *destination);
+void OperandCopy(char *source, char *destination);
 unsigned int ExtractIntFromStr(char* string);
 bool ContainsParentheses(char* string);
-bool MathFunc(unsigned long long val_1, unsigned long long val_2,
+bool PerformArithmetic(unsigned long long val_1, unsigned long long val_2,
               char operation, unsigned int modulus, unsigned int* result);
-bool GetNumbers(char* left, char* right, unsigned long long* left_val_ptr,
-                unsigned long long* right_val_ptr);
+bool GetNumbersFromExpression(char* left, char* right,
+                              unsigned long long* left_val_ptr,
+                              unsigned long long* right_val_ptr);
+void Func(unsigned int modulus, char* left, char* right,
+          unsigned long long* left_val_ptr,
+          unsigned long long* right_val_ptr,
+          unsigned int* expression_result);
 /* -------------------------------------------------------- */
 
 /*
    calculate_modular_expression - calculates a modular expression in the form
-   (
+   MODULUS (A [OPERATOR] B).
 */
 bool calculate_modular_expression(unsigned int modulus,
                                   char* expression,
                                   unsigned int* expression_result)
 {
-    char left[MAX_EXPRESSION_LENGTH] = {0};
-    char right[MAX_EXPRESSION_LENGTH] = {0};
-    char* op = SplitExpression(expression+1);
+    char left[MAX_EXPRESSION_LENGTH+1] = {0};
+    char right[MAX_EXPRESSION_LENGTH+1] = {0};
+    char* operation = SplitExpression(expression+1);
     unsigned long long left_val = 0, right_val = 0;
-    while (*op == '(')
+    while (*operation == '(')
     {
-        op--;
+        operation--;
     }
-    while (*op == ')')
+    while (*operation == ')')
     {
-        op++;
+        operation++;
     }
 
-    StringCopy((expression+1), left);
-    StringCopy((op+1), right);
+    OperandCopy((expression+1), left);
+    OperandCopy((operation+1), right);
 
-    if (GetNumbers(left, right, &left_val, &right_val) == false)
+    if (!GetNumbersFromExpression(left, right, &left_val, &right_val))
     {
         return false;
     }
 
-    if (left_val == 0)
+    Func(modulus, left, right, &left_val, &right_val, expression_result);
+
+    return PerformArithmetic(left_val, right_val, *operation, modulus,
+                             expression_result);
+}
+
+
+
+void Func(unsigned int modulus, char* left, char* right,
+          unsigned long long* left_val_ptr,
+          unsigned long long* right_val_ptr,
+          unsigned int* expression_result)
+{
+    if (*left_val_ptr == 0)
     {
         calculate_modular_expression(modulus, left, expression_result);
-        left_val = *expression_result;
+        *left_val_ptr = *expression_result;
     }
 
-    if (right_val == 0)
+    if (*right_val_ptr == 0)
     {
         calculate_modular_expression(modulus, right, expression_result);
-        right_val = *expression_result;
+        *right_val_ptr = *expression_result;
     }
-
-    return MathFunc(left_val, right_val, *op, modulus, expression_result);
 }
 
 
@@ -128,8 +144,15 @@ int main()
     return 0;
 }
 
-bool GetNumbers(char* left, char* right, unsigned long long* left_val_ptr,
-                unsigned long long* right_val_ptr)
+/*This function, given pointers to the left and right operands of an
+expression and pointers to the corresponding value variables, checks if
+each operand is a single number. If an operand is a single number,
+its value is extracted from the string and saved to the value variable.
+The function returns false if the operand is not a number. If the operand
+is a legal integer (positive), the function returns true.*/
+bool GetNumbersFromExpression(char* left, char* right,
+                              unsigned long long* left_val_ptr,
+                              unsigned long long* right_val_ptr)
 {
     if (ContainsParentheses(left) == false)
     {
@@ -153,8 +176,9 @@ bool GetNumbers(char* left, char* right, unsigned long long* left_val_ptr,
 }
 
 
-
-void StringCopy(char *source, char *destination)
+/*This function, given a source string and a destination string, copies
+a single operand (left or right) from the source to the destination.*/
+void OperandCopy(char *source, char *destination)
 {
     int parentheses_counter = 0;
     do
@@ -179,6 +203,9 @@ void StringCopy(char *source, char *destination)
     *(destination) = '\0';
 }
 
+/*This function, given an expression with two operands separated by an
+operator, finds the address of the operator (the "middle" of the
+expression) and returns it.*/
 char* SplitExpression(char* expression)
 {
     int parentheses_counter = 0;
@@ -211,6 +238,10 @@ char* SplitExpression(char* expression)
     return expression;
 }
 
+/*This function, given a string, extracts an unsigned integer from the
+string and returns it. If no integer is found, the function returns 0.
+(Returning 0 is based on the assumption that all numbers handled will be
+ positive.)*/
 unsigned int ExtractIntFromStr(char* string)
 {
     unsigned int number = 0;
@@ -223,6 +254,9 @@ unsigned int ExtractIntFromStr(char* string)
     return number;
 }
 
+
+/*This function, given a string, reads through it and checks whether or not
+it contains parentheses.*/
 bool ContainsParentheses(char* string)
 {
     while (*string != '\0')
@@ -240,9 +274,10 @@ bool ContainsParentheses(char* string)
 
 /*This function, given two positive values, the operator between them,
 the modulus, and a pointer to the result, calculates the result of the
-mathematical operation and stores it in the result variable.*/
-
-bool MathFunc(unsigned long long val_1, unsigned long long val_2,
+mathematical operation and stores it in the result variable.
+The function returns false if the operator is not '+' or '*'.
+Otherwise, it returns true. */
+bool PerformArithmetic(unsigned long long val_1, unsigned long long val_2,
               char operation, unsigned int modulus, unsigned int* result)
 {
     val_1 = val_1%modulus;
